8039ddff0192eba3823023a9b48770f9798b0d01
Rohan Sharma <sharma.rohan07@proton.me>
2026-03-11 17:52:11 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,30 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut command_input = String::new();
         io::stdin().read_line(&mut command_input);
-        commandParse(&command_input);
+        let command_return = commandParse(&command_input);
+        match command_return {
+            Ok(-1) => break,
+            Ok(_) => continue,
+            Err(_) => continue
+        }
     }
 }
 
-fn commandParse(command_input: &str) {
-    let mut inbuilt_commands = HashMap::<&str, fn()>::new();
+fn commandParse(command_input: &str) -> Result<i32, String> {
+    let mut inbuilt_commands = HashMap::<&str, fn()->Result<i32, String>>::new();
+    inbuilt_commands.insert("exit", exit_program);
     let command_name = command_input.split_whitespace().next();
     let function_pointer = match command_name {
         Some(command_name) => inbuilt_commands.get(command_name),
-        None => return
+        None => return Err("No Command".to_string())
     };
     match function_pointer {
-        Some(function_pointer) => function_pointer(),
-        None => println!("{}: command not found", command_name.unwrap())
+        Some(function_pointer) => return (function_pointer()),
+        None => {println!("{}: command not found", command_name.unwrap()); return Err("Command not found".to_string())}
     };
-    io::stdout().flush().unwrap();
+    // io::stdout().flush().unwrap();
+}
+
+fn exit_program() -> Result<i32, String> {
+    Ok(-1)
 }
