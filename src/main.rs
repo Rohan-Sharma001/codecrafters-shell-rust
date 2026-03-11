4f88ed47466ef5bb2b940fccb95b3698fb7f9575
Rohan Sharma <sharma.rohan07@proton.me>
2026-03-11 22:11:00 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, io::Read};
+use std::{collections::HashMap, env::Args, io::Read, str::SplitWhitespace, vec};
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
@@ -10,6 +10,7 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut command_input = String::new();
         io::stdin().read_line(&mut command_input);
+        command_input.trim();
         let command_return = commandParse(&command_input);
         match command_return {
             Ok(-1) => break,
@@ -19,21 +20,42 @@ fn main() {
     }
 }
 
+fn separator(command: &str) -> Vec<&str> {
+    let vector_of_args: Vec<&str> = command.split_whitespace().collect();
+    return vector_of_args;
+}
+
 fn commandParse(command_input: &str) -> Result<i32, String> {
-    let mut inbuilt_commands = HashMap::<&str, fn()->Result<i32, String>>::new();
+    let mut inbuilt_commands = HashMap::<&str, fn(Vec<&str>)->Result<i32, String>>::new();
     inbuilt_commands.insert("exit", exit_program);
-    let command_name = command_input.split_whitespace().next();
+    inbuilt_commands.insert("echo", echo);
+
+    // let mut parts = command_input.splitn(2, char::is_whitespace);
+
+    let arguments = separator(command_input);
+    let command_name = arguments.get(0);
+    
     let function_pointer = match command_name {
         Some(command_name) => inbuilt_commands.get(command_name),
         None => return Err("No Command".to_string())
     };
     match function_pointer {
-        Some(function_pointer) => return (function_pointer()),
+        Some(function_pointer) => return (function_pointer(arguments)),
         None => {println!("{}: command not found", command_name.unwrap()); return Err("Command not found".to_string())}
     };
-    // io::stdout().flush().unwrap();
+    
 }
 
-fn exit_program() -> Result<i32, String> {
+fn exit_program(arg_array: Vec::<&str>) -> Result<i32, String> {
     Ok(-1)
+}
+
+fn echo(arg_array: Vec::<&str>) -> Result<i32, String> {
+    // println!("{}", arg_array.len());
+    for i in 1..arg_array.len() {
+        if i > 1 {print!(" ");}
+        print!("{}", arg_array[i]);
+    }
+    print!("\n");
+    Ok(0)
 }
